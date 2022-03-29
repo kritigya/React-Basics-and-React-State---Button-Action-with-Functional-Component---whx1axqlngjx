733346e30ede14aee0f04b2800350344efc7233a
kritigya <97790296+kritigya@users.noreply.github.com>
2022-03-29 16:13:11 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
-import React, {Component, useState} from "react";
-import "./../styles/App.css";
+import React, { Component } from 'react';
+import ReactDOM from "react-dom";
 
-function App() {
-  return (
-    <div id="main">
-      // Do not alter the main div
-    </div>
-  );
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isShow: false
+    }
+    this.createText = this.createText.bind(this);
+  }
+
+
+  createText() {
+    this.setState({ isShow: true }) 
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <button id='click' onClick={this.createText}>Click</button>
+        {this.state.isShow && <p id='para'>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
+      </div>
+    );
+  }
 }
 
-
+ReactDOM.render(<App />, document.getElementById('app'))
 export default App;
